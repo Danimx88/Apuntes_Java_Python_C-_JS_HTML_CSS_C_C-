84240991e887802242ffae9f14c3ad91755978ca
Danimx88 <105736149+Danimx88@users.noreply.github.com>
2022-07-28 18:34:36 -0500

    Create Variables.py

---
+++ CursoPyton/Seccion_1_al_10/Variables.py
@@ -0,0 +1,55 @@
+print('Este es mi saludo desde "Python"')
+MyVariable = "Daniel"
+print (MyVariable)
+MyVariable = 10
+print(MyVariable)
+x = 10
+y = 2
+z = x + y
+print(10)
+print(2)
+print(z)
+print(id(x)) #direccion en memoria
+print(id(y)) #direccion en memoria
+print(id(z)) #direccion en memoria
+
+nombre = 'Daniel'
+telefono = 5575005891
+correo = 'r.r.d.o@hotmail.com'
+
+print(nombre)
+print(telefono)
+print(correo)
+print(type(nombre))
+print(type(telefono))
+print(type(correo))
+
+x = False
+print(x)
+print(type(x))
+
+#String (String)
+miGrupoFavorito = 'Aerosmith' + ' The best Rock Band'
+print("Mi grupo favorito = " + miGrupoFavorito)
+
+t = 2
+t+= 2
+print(t)
+
+MiVar = False
+print(MiVar)
+
+MiVar = 1 < 2
+print(MiVar)
+
+resultado = input("Ingrese dato: ")
+print("Dato ingresado: " + resultado) # para String "+"
+print("Fin de ingreso")
+
+numero1 = int(input("Ingrese primer numero: ")) #envolver en tipo entero
+numero2 = int(input("Ingrese segundo numero: "))
+sum = numero1 + numero2
+print("La suma de los dos numeros ingresados es: ", sum) # Para entero ","
+
+dia = int(input("Como estyvo tu dia: "))
+print("Tu dia estuvo de ", dia)
