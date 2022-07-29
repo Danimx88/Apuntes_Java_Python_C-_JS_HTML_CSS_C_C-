61e41a29cc80a3df8142c19f16550e3e47657fd5
Danimx88 <105736149+Danimx88@users.noreply.github.com>
2022-07-29 06:34:07 -0500

    Create rectangulo.py

---
+++ CursoPyton/Seccion_1_al_10/rectangulo.py
@@ -0,0 +1,6 @@
+alto = int(input('Proporciona el alto del rectangulo: '))
+ancho = int(input('Proporciona el ancho de rectangulo: '))
+area = alto * ancho
+perimetro = (alto + ancho) * 2
+print('Area:', area)
+print('Perimetro:', perimetro)
