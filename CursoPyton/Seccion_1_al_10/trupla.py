6a956149e2aee813aef23ce42cad1d5336516ac0
Danimx88 <105736149+Danimx88@users.noreply.github.com>
2022-07-30 01:15:03 -0500

    Create trupla.py

---
+++ CursoPyton/Seccion_1_al_10/trupla.py
@@ -0,0 +1,10 @@
+#Definir tupla
+##Las tuplas ya no permiten modificar, son inmutables
+frutas = ('Naranja','Mango','Platano') #trupla entre ()
+#saber el rango
+print(len(frutas))
+#acceder a un elemento en particular
+#print(frutas[0])
+print(frutas[2])
+print(frutas[-2])
+print(frutas[0:1])
