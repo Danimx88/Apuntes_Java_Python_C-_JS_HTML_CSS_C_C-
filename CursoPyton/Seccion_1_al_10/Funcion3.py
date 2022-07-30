0160e5b57bfa4ced814fe503d659892ac3f51469
Danimx88 <105736149+Danimx88@users.noreply.github.com>
2022-07-30 02:48:15 -0500

    Create Funcion3.py

---
+++ CursoPyton/Seccion_1_al_10/Funcion3.py
@@ -0,0 +1,13 @@
+def suma_Funcion(a, b):
+    return a+b
+
+def resta_Funcion(x, y):
+    return x-y
+
+resultado = suma_Funcion(8, 8)
+print(f'El resultado de la suma es igual: {resultado}')
+
+x = int(input('Ingrese el valor de x: '))
+y = int(input('Ingrese el valor de y: '))
+resta = resta_Funcion(x, y)
+print(
