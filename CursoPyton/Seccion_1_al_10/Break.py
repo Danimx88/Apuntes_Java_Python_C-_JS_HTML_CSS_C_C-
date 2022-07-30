9b6180eb2572ebb967dd51e5c41e66ce6d3e502e
Danimx88 <105736149+Danimx88@users.noreply.github.com>
2022-07-29 23:41:54 -0500

    Create Break.py

---
+++ CursoPyton/Seccion_1_al_10/Break.py
@@ -0,0 +1,6 @@
+for letra in 'Holapa': ##para la cadena letra
+    if letra == 'a': ##Si en la cadena es igual a la letra a
+        print(f'Letra encontrada:  {letra}') ## la letra encontra : resultado
+        break
+else:
+    print('Fin de ciclo for')
