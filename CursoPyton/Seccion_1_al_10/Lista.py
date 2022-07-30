d6e93e8ef43c7e1c5c13e7194591021bdd475eda
Danimx88 <105736149+Danimx88@users.noreply.github.com>
2022-07-29 23:51:48 -0500

    Create Lista.py

---
+++ CursoPyton/Seccion_1_al_10/Lista.py
@@ -0,0 +1,8 @@
+#definir una lista de tipo Str
+nombre = ['Daniel' ,'Omar' ,' Romero' ,'Reyes']
+#imprimir la lista nombres
+print(nombre)
+## accder a los elementos de una lista
+print(nombre[0])
+print(nombre[2])
+print(nombre[-2])
