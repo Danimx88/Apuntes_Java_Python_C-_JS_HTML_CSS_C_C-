ba47de781a2bba6e9ec3aa8b57b964df82997205
Danimx88 <105736149+Danimx88@users.noreply.github.com>
2022-07-30 02:57:53 -0500

    Create FuncionVariables.py

---
+++ CursoPyton/Seccion_1_al_10/FuncionVariables.py
@@ -0,0 +1,7 @@
+def mi_Fun_Nombres(*nombres):  #se pone el * ya que no se sabe cuantos valores se van a tener
+    for nombre in nombres:
+        print(nombre)
+
+mi_Fun_Nombres('Daniel','Omar')
+
+mi_Fun_Nombres('Jorsh','Daniel','Kevin','Iker')
