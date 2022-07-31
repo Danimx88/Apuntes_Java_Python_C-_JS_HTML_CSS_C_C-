fa54fad4e4187d4b40bda110cc92ceec5bc35f2f
Danimx88 <105736149+Danimx88@users.noreply.github.com>
2022-07-31 05:32:44 -0500

    Create Prueba_conecion_bd_posgresql.py

---
+++ CursoPyton/Seccion_1_al_10/Prueba_conecion_bd_posgresql.py
@@ -0,0 +1,18 @@
+import psycopg2
+
+conexion = psycopg2.connect( ##crear objeto conexion
+    user='postgres',
+    password='admin',
+    host='127.0.0.1',
+    port='5432',
+    database='test_db'
+    )
+
+cursor = conexion.cursor()#crear un objeto del tipo cursos
+sentencia = 'SELECT * FROM persona' ##sentencia que se quiere ejecutar
+cursor.execute(sentencia)##mandar a llamar el metodo execute con ayuda del objeto cursor
+registros = cursor.fetchall()##recuperar todos los registros con fetchall
+print(registros)##imprimir
+
+cursor.close()##cerrar cursos 
+conexion.close()##cerrar conexion
