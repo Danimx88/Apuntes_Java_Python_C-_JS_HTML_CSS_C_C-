74633a3ab1f464ad6ac827f8aedcf337509b9e37
Danimx88 <105736149+Danimx88@users.noreply.github.com>
2022-07-31 07:27:56 -0500

    Create EliminarRegistroBD.py

---
+++ CursoPyton/Seccion_1_al_10/EliminarRegistroBD.py
@@ -0,0 +1,21 @@
+import psycopg2
+#Actualizar un registro
+conexion = psycopg2.connect( user='postgres',password='admin',host='127.0.0.1',port='5432',database='test_db'
+    )
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'DELETE FROM persona WHERE id_persona = %s' #codigo para eliminar un id(registro)
+            valores = (12,) #(registro eliminar de id_persona)
+            cursor.execute(sentencia, valores) ##se llamada de nuevo a executemany
+            registros_eliminados = cursor.rowcount
+            print(f'Registros eliminados: {registros_eliminados}')
+except Exception as e:
+    print(f'Ocurrio un error {e}')
+finally:
+    conexion.close()
+
+
+
+cursor.close()##cerrar cursos
+conexion.close()##cerrar conexion
