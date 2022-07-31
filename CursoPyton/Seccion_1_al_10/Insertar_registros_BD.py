c1666d9942db26453c3a5fcb6e1c35776dab36ac
Danimx88 <105736149+Danimx88@users.noreply.github.com>
2022-07-31 06:22:46 -0500

    Create Insertar_registros_BD.py

---
+++ CursoPyton/Seccion_1_al_10/Insertar_registros_BD.py
@@ -0,0 +1,23 @@
+import psycopg2
+
+conexion = psycopg2.connect( user='postgres',password='admin',host='127.0.0.1',port='5432',database='test_db'
+    )
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'
+            valores = ('Carlos','Lara','clara@gmail.com')
+            cursor.execute(sentencia, valores)
+            conexion.commit()#commit guardara los cambios en la base de datos
+            ##Al usar with, el commit se ejecuta automático, así que no hay que colocarse manuelmente
+            registros_insertados = cursor.rowcount #rowcount nos dice cuantos registros se han incertado
+            print(f'Registros insertados: {registros_insertados}')
+except Exception as e:
+    print(f'Ocurrio un error {e}')
+finally:
+    conexion.close()
+
+
+
+cursor.close()##cerrar cursos
+conexion.close()##cerrar conexion
