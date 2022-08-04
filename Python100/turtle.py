bb0c8677748873f4d64aae0abdee408de6e3e486
Danimx88 <105736149+Danimx88@users.noreply.github.com>
2022-08-03 21:33:24 -0500

    Create turtle.py

---
+++ Python100/turtle.py
@@ -0,0 +1,50 @@
+import random
+import turtle
+from turtle import Turtle, Screen
+tim =  Turtle()
+
+screen = Screen()
+tim.forward(100)
+tim.left(90)
+tim.forward(100)
+tim.left(90)
+tim.forward(100)
+tim.right(90)
+tim.forward(100)
+tim.right(90)
+tim.forward(200)
+tim.right(90)
+tim.forward(200)
+tim.right(180)
+tim.forward(200)
+tim.right(90)
+tim.forward(150)
+tim.right(90)
+tim.forward(100)
+tim.right(90)
+tim.forward(150)
+tim.right(180)
+tim.forward(150)
+tim.right(90)
+tim.forward(200)
+tim.right(90)
+tim.forward(350)
+tim.right(90)
+tim.forward(100)
+
+
+screen.exitonclick()
+#turtle.speed(1000)
+x = 1
+"""
+while x < 400:
+    r = random.randint(0,255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    turtle.colormode(255)
+    turtle.pencolor(r, g, b)
+    turtle.forward(10 + x)
+    turtle.right(200)
+
+    x +=1;
+"""
