c95e8f1ce5759f8a6ecf52117de70545f890a20b
Danimx88 <105736149+Danimx88@users.noreply.github.com>
2022-08-03 22:55:27 -0500

    Create turtle2.py

---
+++ Python100/turtle2.py
@@ -0,0 +1,18 @@
+import turtle
+
+
+
+from turtle import *
+color('red', 'yellow')
+begin_fill()
+forward(200)
+goto(2,2)
+
+#while True:
+#    forward(200)
+#    goto(2,1)
+#    left(170)
+#    if abs(pos()) < 1:
+#        break
+#end_fill()
+#done()
