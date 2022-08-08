9c2236a29416b0c4148654f375d2f0d5469a772d
Danimx88 <105736149+Danimx88@users.noreply.github.com>
2022-08-07 23:24:29 -0500

    Create TablasMultiplicar.java

---
+++ Java/TablasMultiplicar.java
@@ -0,0 +1,116 @@
+public class TablasDeMultiplicar {
+    public static void main(String[] args) {
+        int[] tabla = new int[11];
+
+
+        int totalNumber = tabla.length;
+        System.out.println("Tabla del 1 \n");
+        for (int k = 1; k < totalNumber; k++){
+            tabla[k] = k*1;
+        }
+
+        for (int k = 0; k < totalNumber; k++){
+            System.out.println("1 X "+ (k) + " = " + tabla[k]);
+        }
+
+        System.out.println(" ");
+        System.out.println("Tabla del 2 \n");
+
+        for (int k = 1; k < totalNumber; k++){
+            tabla[k] = k*2;
+        }
+
+        for (int k = 0; k < totalNumber; k++){
+            System.out.println("2 X "+ (k) + " = " + tabla[k]);
+        }
+
+        System.out.println(" ");
+        System.out.println("Tabla del 3 \n");
+
+        for (int k = 1; k < totalNumber; k++){
+            tabla[k] = k*3;
+        }
+
+        for (int k = 0; k < totalNumber; k++){
+            System.out.println("3 X "+ (k) + " = " + tabla[k]);
+        }
+
+        System.out.println(" ");
+        System.out.println("Tabla del 4 \n");
+
+        for (int k = 1; k < totalNumber; k++){
+            tabla[k] = k*4;
+        }
+
+        for (int k = 0; k < totalNumber; k++){
+            System.out.println("4 X "+ (k) + " = " + tabla[k]);
+        }
+
+        System.out.println(" ");
+        System.out.println("Tabla del 5 \n");
+
+        for (int k = 1; k < totalNumber; k++){
+            tabla[k] = k*5;
+        }
+
+        for (int k = 0; k < totalNumber; k++){
+            System.out.println("5 X "+ (k) + " = " + tabla[k]);
+        }
+
+        System.out.println(" ");
+        System.out.println("Tabla del 6 \n");
+
+        for (int k = 1; k < totalNumber; k++){
+            tabla[k] = k*6;
+        }
+
+        for (int k = 0; k < totalNumber; k++){
+            System.out.println("6 X "+ (k) + " = " + tabla[k]);
+        }
+
+        System.out.println(" ");
+        System.out.println("Tabla del 7 \n");
+
+        for (int k = 1; k < totalNumber; k++){
+            tabla[k] = k*7;
+        }
+
+        for (int k = 0; k < totalNumber; k++){
+            System.out.println("7 X "+ (k) + " = " + tabla[k]);
+        }
+
+        System.out.println(" ");
+        System.out.println("Tabla del 8 \n");
+
+        for (int k = 1; k < totalNumber; k++){
+            tabla[k] = k*8;
+        }
+
+        for (int k = 0; k < totalNumber; k++){
+            System.out.println("8 X "+ (k) + " = " + tabla[k]);
+        }
+
+        System.out.println(" ");
+        System.out.println("Tabla del 9 \n");
+
+        for (int k = 1; k < totalNumber; k++){
+            tabla[k] = k*9;
+        }
+
+        for (int k = 0; k < totalNumber; k++){
+            System.out.println("9 X "+ (k) + " = " + tabla[k]);
+        }
+
+        System.out.println(" ");
+        System.out.println("Tabla del 10 \n");
+
+        for (int k = 1; k < totalNumber; k++){
+            tabla[k] = k*10;
+        }
+
+        for (int k = 0; k < totalNumber; k++){
+            System.out.println("10 X "+ (k) + " = " + tabla[k]);
+        }
+
+    }
+}
